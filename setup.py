9eabd17b7027427350395f552df4e3e98ac5be7d
Feth Arezki <feth@majerti.fr>
2016-06-06 14:55:58 +0200

    bump minor version 0.1.4
    I'm making a git tag, which is a good practice so you know where the version
    you're building comes from.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ config = {
     'url': 'https://github.com/majerteam/stringimporter',
     'download_url': 'https://github.com/majerteam/stringimporter',
     'author_email': 'tech@majerti.fr',
-    'version': '0.1.3',
+    'version': '0.1.4',
     'packages': ['stringimporter'],
     'setup_requires': ['pytest-runner'],
     'tests_require': ['pytest'],
